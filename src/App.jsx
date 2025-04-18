e6dd8a35533736fa505b9ddac494b5a077d2bb67
Tarık İslamoğlu <tarikislamoglu8@gmail.com>
2025-04-18 13:47:25 +0300

    finalfest

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import { useState } from 'react'
-import './styles.css'
-import festivalData from './data/festivalData'
-import BuyTickets from './components/BuyTickets'
-import Footer from './components/Footer'
-import PosterSection from './components/PosterSection'
+import { useEffect, useState } from "react";
+import "./styles.css";
+import festivalData from "./data/festivalData";
+import BuyTickets from "./components/BuyTickets";
+import Footer from "./components/Footer";
+import PosterSection from "./components/PosterSection";
 
 export default function App() {
+  const [festivals, setFestivals] = useState({
+    firstDay: festivalData.filter((fest) => fest.date === "1 Haziran"),
+    secondDay: festivalData.filter((fest) => fest.date === "2 Haziran"),
+    thirdDay: festivalData.filter((fest) => fest.date === "3 Haziran"),
+  });
+
   return (
-    <div className='wrapper'>
+    <div className="wrapper">
       <h1>Festival</h1>
 
-      <PosterSection date='1 Haziran' festivalData={festivalData} />
+      <PosterSection date="1 Haziran" festivalData={festivals.firstDay} />
 
-      <PosterSection date='2 Haziran' festivalData={festivalData} />
+      <PosterSection date="2 Haziran" festivalData={festivals.secondDay} />
 
-      <PosterSection date='3 Haziran' festivalData={festivalData} />
+      <PosterSection date="3 Haziran" festivalData={festivals.thirdDay} />
 
       <BuyTickets />
       <Footer />
     </div>
-  )
+  );
 }
--- src/components/PosterSection.jsx
+++ src/components/PosterSection.jsx
@@ -1,7 +1,65 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function PosterSection({ date, festivalData }) {
-  /*Challenge
+  const [festivals, setFestivals] = useState({
+    oneImp: festivalData.filter((fest) => fest.importance === 1),
+    twoImp: festivalData.filter((fest) => fest.importance === 2),
+    threeImp: festivalData.filter((fest) => fest.importance === 3),
+    fourImp: festivalData.filter((fest) => fest.importance === 4),
+  });
+
+  return (
+    <div className="lineup-container">
+      <div className="flex flex-col space-y-5 w-full">
+        <h3 className="p-2 rounded-md text-white text-[20px] m-0 bg-[#111111a1] text-center">
+          {date}
+        </h3>
+        <div>
+          <p className="giant">Süper Büyük Önemli Gruplar:</p>
+          {festivals.oneImp.map((fest, index) => {
+            return (
+              <p className="giant">
+                {index + 1}:{fest.name}
+              </p>
+            );
+          })}
+        </div>
+        <div>
+          <p className="big">Biraz Daha Az Önemli Gruplar:</p>
+          {festivals.twoImp.map((fest, index) => {
+            return (
+              <p className="big">
+                {index + 1}:{fest.name}
+              </p>
+            );
+          })}
+        </div>
+        <div>
+          <p className="medium">Orta Dereceli Gruplar:</p>
+          {festivals.threeImp.map((fest, index) => {
+            return (
+              <p className="medium">
+                {index + 1}:{fest.name}
+              </p>
+            );
+          })}
+        </div>
+        <div>
+          <p className="small">O kadar da önemli olmayan bir gruplar:</p>
+          {festivals.fourImp.map((fest, index) => {
+            return (
+              <p className="small">
+                {index + 1}:{fest.name}
+              </p>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*Challenge
 	
 	Müzikseverlerin bu yılki Festival'de hangi gün hangi grupların çalacağını bilmesi gerekiyor. Sizin göreviniz bu PosterSection'ı aşağıdaki gibi bu işi yapabilecek hale getirmek: 
 	
@@ -12,25 +70,5 @@ export default function PosterSection({ date, festivalData }) {
 		3. Bir grubun önem değerinin 1, 2, 3 veya 4 olmasına bağlı olarak, paragraf elementi sırasıyla "giant", "big", "medium" veya "small" class adını almalıdır. 
 		
 		4. Bu görevleri doğru bir şekilde tamamlarsanız, 1 Haziran bölümünde yalnızca 1 Haziran 'da çalan grupları görmelisiniz; en önemlileri üstte büyük puntolarla, daha az önemlileri ise altta daha küçük puntolarla yer almalıdır. Aynı şey 2 ve 3 Haziran için de geçerlidir. Sayfanın altında otomatik olarak bir footer da görünmelidir. 
-
-
-
 		   
  */
-
-  return (
-    <div className='lineup-container'>
-      <div className='day-container'>
-        <h3>{date}</h3>
-      </div>
-
-      <p className='giant'>Süper Büyük Önemli Grup</p>
-      <p className='giant'>Başka Biri</p>
-      <p className='big'>Biraz Daha Az Önemli Grup</p>
-      <p className='big'>Başka Biri</p>
-      <p className='medium'>Orta Dereceli Grup</p>
-      <p className='medium'>Başka Biri</p>
-      <p className='small'>O kadar da önemli olmayan bir grup</p>
-    </div>
-  )
-}
